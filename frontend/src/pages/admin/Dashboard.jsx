603f0dab5c1aaedf053e4ae78e6b5826df39dc99
pawan0110 <meroy0075@gmail.com>
2025-12-03 22:44:51 +0530

    .

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,41 +1,94 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import {BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid} from "recharts"; 
-import { FaArrowLeftLong } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid,
+} from "recharts";
+import { FaArrowLeftLong } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import img from "../../assets/empty.jpg";
 function Dashboard() {
-    const navigate = useNavigate()
-    const {userData} = useSelector((state) => state.user);
-    const { creatorCourseData} = useSelector((state) => state.course);
+  const navigate = useNavigate();
+  const { userData } = useSelector((state) => state.user);
+  const { creatorCourseData } = useSelector((state) => state.course);
 
-    const courseProgressData = creatorCourseData?.map(course=>({
-        name: course.title.slice(0, 10) + "...",
-        lectures: course.lectures.length || 0
+  const courseProgressData =
+    creatorCourseData?.map((course) => ({
+      name: course.title.slice(0, 10) + "...",
+      lectures: course.lectures.length || 0,
     })) || [];
 
-    const enrollData = creatorCourseData?.map(course => ({
-        name: course.title.slice(0, 10) + "...",
-        enrolled: course.enrolledStudents?.length || 0
+  const enrollData =
+    creatorCourseData?.map((course) => ({
+      name: course.title.slice(0, 10) + "...",
+      enrolled: course.enrolledStudents?.length || 0,
     })) || [];
 
-    const totalEarnings = creatorCourseData?.reduce((sum, course) => {
-        const studentCount = course.enrolledStudents?.length || 0;
-        const courseRevenue = course.price ? course.price*studentCount : 0;
-        return sum + courseRevenue;
-    },0) || 0;
+  const totalEarnings =
+    creatorCourseData?.reduce((sum, course) => {
+      const studentCount = course.enrolledStudents?.length || 0;
+      const courseRevenue = course.price ? course.price * studentCount : 0;
+      return sum + courseRevenue;
+    }, 0) || 0;
 
   return (
-    <div className='flex min-h-screen bg-gray-100'>
-    <FaArrowLeftLong className='w-[22px] absolute top-[10%] left-[10%] h-[22px] cursor-pointer onClick={() => naviagte("/")}'/>
-    <div className='w-full px-6 py-10 bg-gray space-y-10'>
-    <div className='max-w-5xl mx-auto bg-white rounded-xl shadow-md p-6 flex flex-col md:flx-row items-center gap-6'>
+    <div className="flex min-h-screen bg-gray-100">
+      <FaArrowLeftLong className='w-[22px] absolute top-[10%] left-[10%] h-[22px] cursor-pointer onClick={() => naviagte("/")}' />
+      <div className="w-full px-6 py-10 bg-gray space-y-10">
+        <div className="max-w-5xl mx-auto bg-white rounded-xl shadow-md p-6 flex flex-col md:flx-row items-center gap-6">
+          <img
+            src={userData?.photoUrl || img}
+            alt="Educator"
+            className="w-28 h-28 rounded-full object-cover border-4 border-black shadow-md"
+          />
+          <div className="text-center md:text-left space-y-1">
+            <h1 className="text-2xl font-bold text-gray-800">
+              Welcome, {userData?.name || "Educator"} 👋
+            </h1>
+            <h1
+              className="text-xl font-semibold
+      text-gray-800"
+            >
+              {" "}
+              Total Earning :{" "}
+              <span className="font-light text-gray-900">
+                ₹{totalEarnings.toLocaleString()}
+              </span>
+            </h1>
+            <p className="text-gray-600 text-sm">
+              {userData?.description ||
+                "start creating amazing courses for your students"}
+            </p>
+            <h1
+              className='px-2.5 text-center py-2.5 border-2 bg-black border-black text-white rounded-[10px]
+      text-[15px] font-light items-center justify-center gap-2 cursor-pointer onClick={()=> naviagte("/courses")}'
+            >
+              Create Courses
+            </h1>
+          </div>
+        </div>
 
-    </div>
+        {/* graphs section */}
 
-    </div>
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+         <div className="bg-white rounded-lg shadow-md p-6">
+         <h2 className="text-lg font-semibold mb-4">Course progess (Lectures)</h2>
+         <ResponsiveContainer width="100%" height={300}>
+           <BarChart>
+            
+           </BarChart>
+         </ResponsiveContainer>
 
+         </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
